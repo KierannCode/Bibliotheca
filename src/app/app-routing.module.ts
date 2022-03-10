dad8448ed59c10a6595b68a47cacf02e2780ae0c
KierannCode <kierann.corre@gmail.com>
2022-03-10 13:35:49 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthorListComponent } from './component/author/author-list/author-list.component';
+import { BookListComponent } from './component/book/book-list/book-list.component';
+import { ErrorComponent } from './component/error/error.component';
+import { HomeComponent } from './component/home/home.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { ReaderComponent } from './component/reader/reader.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', pathMatch: 'full', redirectTo: 'home'
+  }, {
+    path: '', component: NavbarComponent, children: [{
+      path: 'error/:code', component: ErrorComponent
+    },{
+      path: 'home', component: HomeComponent
+    }, {
+      path: 'authors', component: AuthorListComponent
+    }, {
+      path: 'books', component: BookListComponent
+    }]
+  }, {
+    path: 'reader', component: ReaderComponent
+  }, {
+    path: '**', redirectTo: 'error/404'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { AuthorListComponent } from './component/author/author-list/author-list.component';
+import { ChapterListComponent } from './component/chapter/chapter-list/chapter-list.component';
+import { BookListComponent } from './component/book/book-list/book-list.component';
+import { HomeComponent } from './component/home/home.component';
+import { ErrorComponent } from './component/error/error.component';
+import { ReaderViewComponent } from './component/reader/reader-view/reader-view.component';
+import { ReaderNavbarComponent } from './component/reader/reader-navbar/reader-navbar.component';
+import { ReaderComponent } from './component/reader/reader.component';
+import { LogInFormComponent } from './component/navbar/log-in-form/log-in-form.component';
+import { SignInDialogComponent } from './component/navbar/sign-in-dialog/sign-in-dialog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AuthorListComponent,
+    ChapterListComponent,
+    BookListComponent,
+    HomeComponent,
+    ErrorComponent,
+    ReaderViewComponent,
+    ReaderNavbarComponent,
+    ReaderComponent,
+    LogInFormComponent,
+    SignInDialogComponent
   ],
   imports: [
     BrowserModule,
