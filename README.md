f30cb9db8b5922247b482fd8bd7a53b32f2d7aa2
KierannCode <kierann.corre@gmail.com>
2022-04-07 09:25:43 +0200

    renamed project

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# IndexFrontend
+# BibliothecaInterface
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.2.5.
 
--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/index-frontend'),
+      dir: require('path').join(__dirname, './coverage/bibliotheca-interface'),
       subdir: '.',
       reporters: [
         { type: 'html' },
